e89fa1e436ef91a10a407783be48f65089fbd7e9
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-06-01 23:27:42 +0700

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header(){
+  return(
+    <header className="border-b p-3 flex justify-between items-center">
+      <span className="font-bold">First React</span>
+      <Navigation />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/Components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HelloWorld(props){
+  return (
+    <h1>Hello {props.name}</h1>
+  )
+}
+
+export default HelloWorld
