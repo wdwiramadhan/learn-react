d6653cf56362e98a14d5ed6a1e404da28e51d53d
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-06-02 23:30:11 +0700

    add router link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import HelloWorld from './Components/HelloWorld';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./views/Home";
+import Product from "./views/Product";
+import About from "./views/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header />
-      <HelloWorld name="Quarantine" />
+      <Router>
+        <Header />
+        <div className="p-3">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/product/:id">
+              <Product />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <Footer />
     </div>
   );
