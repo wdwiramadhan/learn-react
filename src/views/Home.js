e840c446011fadd17ca55b14cb24911f7a45e7f3
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-06-03 21:49:41 +0700

    show all data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "../components/Loader";
+import {Link } from "react-router-dom";
 
 const Home = () => {
-  return(
+  const url = "https://5ed668f6c2ca2300162c6539.mockapi.io/api/v1/product";
+  const [products, setProducts] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
+  let content = null;
+  useEffect(() => {
+    setProducts({
+      loading: true,
+      data: null,
+      error: false,
+    });
+    axios
+      .get(url)
+      .then((res) =>
+        setProducts({
+          loading: true,
+          data: res.data,
+          error: false,
+        })
+      )
+      .catch((err) =>
+        setProducts({
+          loading: false,
+          data: null,
+          error: true,
+        })
+      );
+  }, [url]);
+  if (products.loading) {
+    content = <Loader />;
+  }
+  if(products.error){
+    content = <p>There was an error. Refresh page or try again later.</p>
+  }
+  if (products.data) {
+    content = products.data.map((product,key) => 
+      <div className="border mb-4 rounded overflow-hidden" key={key}>
+        <Link to={`/product/${product.id}`}>
+          <div>
+            <img src={product.images} alt={product.name} />
+          </div>
+        </Link>
+        <div className="p-3">
+          <h3 className="font-bold mb-3">
+            <Link to={`/product/${product.id}`}>{product.name}</Link>
+          </h3>
+          <div className="font-bold mb-1">
+            $ {product.price}
+          </div>
+        </div>
+      </div>
+    )
+  }
+  return (
     <div>
-       <h1>Home</h1>
+      <h1 className="font-bold text-xl mb-3">Best Sellers</h1>
+      {content}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/views/Product.js
+++ src/views/Product.js
@@ -5,8 +5,7 @@ import Loader from "../components/Loader";
 
 const Product = () => {
   const { id } = useParams();
-  const url =
-    "https://5ed668f6c2ca2300162c6539.mockapi.io/api/v1/product/" + id;
+  const url = "https://5ed668f6c2ca2300162c6539.mockapi.io/api/v1/product/" + id;
   const [product, setProduct] = useState({
     loading: false,
     data: null,
@@ -50,7 +49,7 @@ const Product = () => {
           <img src={product.data.images} alt={product.data.name} />
         </div>
         <div className="font-bold text-xl mb-3">$ {product.data.price}</div>
-        <div>{product.data.description}</div>
+        <div>Remember, a Jedi can feel the Force flowing through him. I suggest you try it again, Luke. This time, let go your conscious self and act on instinct. Kid, I've flown from one side of this galaxy to the other. I've seen a lot of strange stuff, but I've never seen anything to make me believe there's one all-powerful Force controlling everything. There's no mystical energy field that controls my destiny. It's all a lot of simple tricks and nonsense.</div>
       </div>
     );
   }
